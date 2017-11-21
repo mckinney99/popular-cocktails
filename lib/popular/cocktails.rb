ef688af4827f5e13e7d548f15f604d8224189022
Eric McKinney <mckinney99@gmail.com>
2017-11-21 11:57:32 -0600

    Created new gem

---
+++ lib/popular/cocktails.rb
@@ -0,0 +1,7 @@
+require "popular/cocktails/version"
+
+module Popular
+  module Cocktails
+    # Your code goes here...
+  end
+end
---
+++ lib/popular/cocktails/version.rb
@@ -0,0 +1,5 @@
+module Popular
+  module Cocktails
+    VERSION = "0.1.0"
+  end
+end
